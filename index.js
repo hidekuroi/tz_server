ac4e6eef1128eedfec4187ab4b495b544bf31887
hidekuroi <kiryil121@yandex.ru>
2022-10-23 01:33:57 +0300

    cors fix

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const router = require('./routes/itemRouter')
-const cors = require('cors')
 
 const PORT = process.env.PORT || 8080
 
 const app = express()
+const corsMiddleware = require('./middleware/corsMiddleware.js')
 
-app.use(cors())
+app.use(corsMiddleware)
 app.use(express.json())
 app.use('/api', router)
 
